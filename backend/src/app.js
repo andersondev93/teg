a66b85fa0bf8692e5f83bf0a749f2b57da5f6b5c
Anderson Gonçalves <anderson.dev93@gmail.com>
2025-07-27 12:33:36 -0300

    ajuste 3

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,25 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
-app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://teg-alpha.vercel.app/'] , // ou a URL do seu frontend
-}));
 
+// 🔧 Forçar headers CORS manualmente
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://teg-alpha.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // Responde preflight
+  }
+
+  next();
+});
+
+app.use(express.json());
 
 app.use("/auth", authRoutes);
 
